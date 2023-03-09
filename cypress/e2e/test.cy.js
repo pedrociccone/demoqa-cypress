91cd3bce90dd59cf933899ce5f52cff38b31b33f
“Pedro <pedrocicc14@gmail.com>
2023-03-09 10:00:28 -0300

    Updating test.cy.js.

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,17 @@
 describe('template spec', () => {
-  it('passes', () => {
+  // beforeEach('Open website', () => {
+
+  // })
+
+  it('Submit form', () => {
     cy.visit('https://demoqa.com/')
+    cy.get('.category-cards > :nth-child(2)').click()
+    cy.get(':nth-child(2) > .element-list > .menu-list > #item-0').click()
+
+    cy.get('#firstName').type('Pedro')
+    cy.get('#lastName').type('Ciccone')
+    cy.get('#userEmail').type('pedrocicc@gmail.com')
+    cy.xpath('//*[@id="genterWrapper"]/div[2]/div[1]/input').click()
+    cy.get('#userNumber').type('1234567890')
   })
 })
