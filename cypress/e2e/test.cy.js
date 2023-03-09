88ea269d5cbf5eb0cc598796f1e42cb6afd0c50c
“Pedro <pedrocicc14@gmail.com>
2023-03-09 10:20:22 -0300

    Deleting cy.xpath function.

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 describe('template spec', () => {
   // beforeEach('Open website', () => {
 
@@ -11,7 +13,7 @@ describe('template spec', () => {
     cy.get('#firstName').type('Pedro')
     cy.get('#lastName').type('Ciccone')
     cy.get('#userEmail').type('pedrocicc@gmail.com')
-    cy.xpath('//*[@id="genterWrapper"]/div[2]/div[1]/input').click()
+    cy.get('#genterWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').click()
     cy.get('#userNumber').type('1234567890')
   })
 })
